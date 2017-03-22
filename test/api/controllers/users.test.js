b5710dcbd9df7d2d49fd848fa9d5c2fb23173818
Ivonne Terrero <iqcodes21@gmail.com>
2017-03-22 10:27:35 -0700

    creating tests file

---
+++ test/api/controllers/users.test.js
@@ -0,0 +1,6 @@
+const request = require('supertest');
+const app = require('../../../app');
+const knex = require('../../../knex')
+const expect = require('chai').expect;
+
+process.env.NODE_ENV = 'test';
