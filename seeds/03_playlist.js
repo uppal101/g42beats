974283fdb9e1a79d2037f69c89e41405b200d63d
Ivonne Terrero <iqcodes21@gmail.com>
2017-03-23 13:31:09 -0700

    proper data

--- seeds/03_playlist.js
+++ seeds/03_playlist.js
@@ -8,341 +8,477 @@ exports.seed = function(knex, Promise) {
                         id: 1,
                         song_id: 1,
                         user_id: 1,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 2,
                         song_id: 2,
                         user_id: 1,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 3,
                         song_id: 3,
                         user_id: 1,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 4,
                         song_id: 4,
                         user_id: 1,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 5,
                         song_id: 5,
                         user_id: 1,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 6,
                         song_id: 6,
                         user_id: 2,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 7,
                         song_id: 7,
                         user_id: 2,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 8,
                         song_id: 8,
                         user_id: 2,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 9,
                         song_id: 9,
                         user_id: 2,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 10,
                         song_id: 10,
                         user_id: 2,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 11,
                         song_id: 11,
                         user_id: 4,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 12,
                         song_id: 12,
                         user_id: 4,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 13,
                         song_id: 13,
                         user_id: 4,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 14,
                         song_id: 14,
                         user_id: 4,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 15,
                         song_id: 15,
                         user_id: 4,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 16,
                         song_id: 16,
                         user_id: 3,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 17,
                         song_id: 17,
                         user_id: 3,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 18,
                         song_id: 18,
                         user_id: 3,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 19,
                         song_id: 19,
                         user_id: 3,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 20,
                         song_id: 20,
                         user_id: 3,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 21,
                         song_id: 21,
                         user_id: 5,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 22,
                         song_id: 22,
                         user_id: 5,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 23,
                         song_id: 23,
                         user_id: 5,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 24,
                         song_id: 24,
                         user_id: 5,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 25,
                         song_id: 25,
                         user_id: 5,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 26,
                         song_id: 26,
                         user_id: 6,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 27,
                         song_id: 27,
                         user_id: 6,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 28,
                         song_id: 28,
                         user_id: 6,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 29,
                         song_id: 29,
                         user_id: 6,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 30,
                         song_id: 30,
                         user_id: 6,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 31,
                         song_id: 31,
                         user_id: 7,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 32,
                         song_id: 32,
                         user_id: 7,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 33,
                         song_id: 33,
                         user_id: 7,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 34,
                         song_id: 34,
                         user_id: 7,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 35,
                         song_id: 35,
                         user_id: 7,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 36,
                         song_id: 36,
                         user_id: 8,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 37,
                         song_id: 37,
                         user_id: 8,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 38,
                         song_id: 38,
                         user_id: 8,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 39,
                         song_id: 39,
                         user_id: 9,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 40,
                         song_id: 40,
                         user_id: 9,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 41,
                         song_id: 41,
                         user_id: 9,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 42,
                         song_id: 42,
                         user_id: 9,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 43,
                         song_id: 43,
                         user_id: 9,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 44,
                         song_id: 44,
                         user_id: 10,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 45,
                         song_id: 45,
                         user_id: 10,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 46,
                         song_id: 46,
                         user_id: 10,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 47,
                         song_id: 47,
                         user_id: 10,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 48,
                         song_id: 48,
                         user_id: 10,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 49,
                         song_id: 49,
                         user_id: 11,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 50,
                         song_id: 50,
                         user_id: 11,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 51,
                         song_id: 51,
                         user_id: 11,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 52,
                         song_id: 52,
                         user_id: 11,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 53,
                         song_id: 53,
                         user_id: 11,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 54,
                         song_id: 54,
                         user_id: 12,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 55,
                         song_id: 55,
                         user_id: 12,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 56,
                         song_id: 56,
                         user_id: 12,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 57,
                         song_id: 57,
                         user_id: 12,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 58,
                         song_id: 58,
                         user_id: 12,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 59,
                         song_id: 59,
                         user_id: 13,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 60,
                         song_id: 60,
                         user_id: 13,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 61,
                         song_id: 61,
                         user_id: 13,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 62,
                         song_id: 62,
                         user_id: 13,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 63,
                         song_id: 63,
                         user_id: 13,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 64,
                         song_id: 64,
                         user_id: 14,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 65,
                         song_id: 65,
                         user_id: 14,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 66,
                         song_id: 66,
                         user_id: 14,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 67,
                         song_id: 67,
                         user_id: 14,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                     {
                         id: 68,
                         song_id: 68,
                         user_id: 14,
+                        created_at: new Date(),
+                        updated_at: new Date()
                     },
                 ])
                 .then(() => {
--- test/api/controllers/users.test.js
+++ test/api/controllers/users.test.js
@@ -1,8 +1,6 @@
 const request = require('supertest');
 const app = require('../../../app');
 const knex = require('../../../knex');
-
-
 const expect = require('mocha').expect;
 
 process.env.NODE_ENV = 'test';
@@ -16,130 +14,26 @@ process.env.NODE_ENV = 'test';
 //         });
 // });
 
-// `beforeEach` is run before each test in a describe
-beforeEach((done) => {
-    knex.seed.run()
-        .then(() => {
-            return Promise.all([
-                knex('users').insert(
-                  {
-                    id: 1,
-                    user_name: 'Hamid',
-                    hashed_password: '$2a$12$Zkvv0auYrrIpd3uMx0IaieYN1rZwyYGmvKMUqZHuefeh7uwY5VAlm'
-                }),
-                knex('users').insert(
-                  {
-                    id: 2,
-                    user_name: 'Josh',
-                    hashed_password: '$2a$12$Zkvv0auYrrIpd3uMx0IaieYN1rZwyYGmvKMUqZHuefeh7uwY5VAlm'
-                  })
-            ])
-        })
-        .then(function() {
-          return knex.raw(`SELECT setval('users_id_seq', (SELECT MAX(id) FROM users))`)
-        })
-        .then(function() {
-            return Promise.all([
-              knex('songs').insert(
-                { id: 1,
-                  song_name: 'Shape Of You',
-                  artist: 'Ed Sheeran'
-                },
-                { id: 2,
-                  song_name: 'Rockabye',
-                  artist: 'Clean Candid'
-                },
-                { id: 3,
-                  song_name: 'Papers',
-                  artist: 'Usher'
-                },
-                { id: 4,
-                  song_name: 'Mercy',
-                  artist: 'Shawn Mendes'
-                }
-              )
-            ])
-        })
-        .then(function() {
-          return knex.raw(`SELECT setval('songs_id_seq', (SELECT MAX(id) FROM songs))`)
-        })
-        .then(function() {
-          return Promise.all([
-            knex('group_members').insert(
-                { id: 1,
-                  group_id: 1,
-                  user_id: 1
-                },
-                { id: 2,
-                  group_id: 1,
-                  user_id: 2
-                }
-              )
-          ])
-        })
-        .then(function() {
-          return knex.raw(`SELECT setval('users_id_seq', (SELECT MAX(id) FROM users))`)
-        })
-        .then(function(){
-          return Promise.all([
-            knex('playlist').insert(
-              { id: 1
-                user_id: 2,
-                song_id: 1
-              },
-              { id: 2
-                user_id: 2,
-                song_id: 2
-              },
-              { id: 3
-                user_id: 1,
-                song_id: 3
-              },
-              { id: 4
-                user_id: 1,
-                song_id: 4
-              }
-            )
-          ])
-        })
-        .then(function() {
-          return knex.raw(`SELECT setval('playlist_id_seq', (SELECT MAX(id) FROM playlist))`)
-        })
-        .then(()=> done());
-});
+describe('GET /users/:id', () => {
+//Do this before every test
+    before((done) => {
+        knex.migrate.latest()
+            .then(function() => {
+                done();
+            })
+            .catch((err) => {
+                done(err);
+            });
+    });
 
-afterEach((done)=> {
-  knex.migrate.rollback()
-  .then(()=> {
-    done(err)
-  });
-});
-
-
-describe('GET /users/:id', function() {
-
-    it('should welcome the authorized user', function(done) {
-        request(app)
-            .get('/:id')
-            .expect(200)
-            .expect(`Welcome, ${username}`, done)
-    })
-})
-
-// it('should respond with application/json', function(done) {
-//   request(app)
-//     .get('/users page')
-//     .expect('Content-Type', /application\json/, done)
-// })
-
-// it('should respond with a paragraph containing :word', function(done) {
-//   request(app)
-//     .get('/word')
-//     .expect('<p>word</p>')
-//
-//   request(app)
-//     .get('/anotherword')
-//     .expect('<p>anotherword</p>', done)
-// })
-// });
-// });
+    // `beforeEach` is run before each test in a describe
+    beforeEach((done) => {
+        knex.seed.run()
+            .then(() => {
+                done();
+            })
+            .catch((err) => {
+                done(err);
+            });
+    });
+  })
