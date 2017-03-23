19382fb3cc3e3358c9fd7e0bfb38817128007ca8
Ivonne Terrero <iqcodes21@gmail.com>
2017-03-23 15:11:39 -0700

    latest test fix

--- test/api/controllers/users.test.js
+++ test/api/controllers/users.test.js
@@ -1,39 +1,54 @@
-const request = require('supertest');
-const app = require('../../../app');
-const knex = require('../../../knex');
-const expect = require('mocha').expect;
+'use strict';
 
 process.env.NODE_ENV = 'test';
-// before((done) => {
-//     knex.migrate.latest()
-//         .then(() => {
-//             done();
-//         })
-//         .catch((err) => {
-//             done(err);
-//         });
-// });
 
-describe('GET /users/:id', () => {
-//Do this before every test
-    before((done) => {
-        knex.migrate.latest()
-            .then(function() => {
-                done();
-            })
-            .catch((err) => {
-                done(err);
-            });
+// const { describe, it } = require('mocha');
+const assert = require('chai').assert;
+const request = require('supertest');
+const knex = require('../../../knex');
+const app = require('../../../app');
+const bcrypt = require('bcrypt');
+
+
+
+describe('users routes', () => {
+
+  // `before` runs once before all tests in a describe
+  before((done) => {
+    knex.migrate.rollback()
+    .then(function(){
+      return knex.migrate.latest()
+    })
+    .then(() => {
+      done();
+    })
+    .catch((err) => {
+      done(err);
+    });
+  });
+
+  // `beforeEach` is run before each test in a describe
+  beforeEach((done) => {
+    knex.seed.run()
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+    //starting to write tests Ivonne
+    it('should respond with user information with the specified id', (done) => {
+      request(index)
+        .get('/users/1')
+        .set('Accept', 'application/json')
+        .expect({
+          id: 1,
+          user_name: 'AlexKrawiec'
+        }, done);
     });
 
-    // `beforeEach` is run before each test in a describe
-    beforeEach((done) => {
-        knex.seed.run()
-            .then(() => {
-                done();
-            })
-            .catch((err) => {
-                done(err);
-            });
-    });
-  })
+
+    ///this one closes all the usres route tests.
+  });
